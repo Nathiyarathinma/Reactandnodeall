0619b978c205be306127c64421d73b8c3b440ffd
Rathinam <626319@cognizant.com>
2019-01-25 18:07:59 +0530

    first commit

---
+++ reactdeploy/build/precache-manifest.bf96796638cb927ebd3cee4b5c17885c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c1e22a39bda57ad92d16",
+    "url": "/static/js/main.c1e22a39.chunk.js"
+  },
+  {
+    "revision": "6ae681515fa7fcd96d57",
+    "url": "/static/js/1.6ae68151.chunk.js"
+  },
+  {
+    "revision": "ae397298655ecc347cd9244c6617cc8a",
+    "url": "/index.html"
+  }
+];
